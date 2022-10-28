09f951377596219cf6f7845f42dfd51d2f0a39db
uzinok <41639106+uzinok@users.noreply.github.com>
2022-10-28 14:48:46 +0300

    Добавляет таймер

--- scripts/main.js
+++ scripts/main.js
@@ -62,13 +62,7 @@ var Details = /*#__PURE__*/function () {
   }]);
 
   return Details;
-}();
-
-if (document.querySelector('.details')) {
-  document.querySelectorAll('.details').forEach(function (elem) {
-    var details = new Details(elem, 300);
-  });
-} // скролл сертификатов
+}(); // скролл сертификатов
 
 
 var about = document.querySelector('.about');
@@ -90,3 +84,73 @@ if (elementInViewport(scrollList)) scrollList.style.transform = 'translateX(-' +
 document.addEventListener('scroll', function () {
   if (elementInViewport(scrollList)) scrollList.style.transform = 'translateX(-' + scrollList.getBoundingClientRect().top / window.innerHeight * 100 + '%)';
 });
+window.addEventListener('load', function () {
+  if (document.querySelector('.details')) {
+    document.querySelectorAll('.details').forEach(function (elem) {
+      var details = new Details(elem, 300);
+    });
+  }
+}); // таймер
+
+document.addEventListener('DOMContentLoaded', function () {
+  var arrTimer = document.querySelectorAll('.timer');
+
+  for (var i = 0; i < arrTimer.length; i++) {
+    timer(arrTimer[i]);
+  }
+}); // сегодняшняя дата
+
+var date = new Date();
+var arrMount = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+
+function timer(timer) {
+  // конечная дата
+  var deadline = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 19, 0, 0, 0); // id таймера
+
+  var timerId = null; // добавление ведущего нуля
+
+  function leadingZero(number) {
+    return number = number < 10 ? '0' + number : number;
+  } // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+
+
+  function countdownTimer() {
+    var diff = deadline.getTime() - new Date().getTime();
+
+    if (diff <= 0) {
+      clearInterval(timerId);
+    } // console.log(new Date());
+
+
+    var hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) : 0;
+    var minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+    var seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+    $hours.textContent = leadingZero(hours);
+    $minutes.textContent = leadingZero(minutes);
+    $seconds.textContent = leadingZero(seconds); // добавить условия для вывода
+
+    if (date.getDate() == deadline.getDate() && date.getMonth() == deadline.getMonth()) {
+      $timerEndDate.textContent = 'сегодня в ' + leadingZero(deadline.getHours()) + ':' + leadingZero(deadline.getMinutes()) + ' по МСК';
+    } else if (deadline.getDate() - date.getDate() == 1 && date.getMonth() == deadline.getMonth()) {
+      $timerEndDate.textContent = 'завтра в ' + leadingZero(deadline.getHours()) + ':' + leadingZero(deadline.getMinutes()) + ' по МСК';
+    } else {
+      $timerEndDate.textContent = deadline.getDate() + ' ' + arrMount[deadline.getMonth()] + ' в ' + leadingZero(deadline.getHours()) + ':' + leadingZero(deadline.getMinutes()) + ' по МСК';
+    }
+  } // получаем элементы, содержащие компоненты даты
+
+
+  var $hours = timer.querySelector('.timer__hours');
+  var $minutes = timer.querySelector('.timer__minutes');
+  var $seconds = timer.querySelector('.timer__seconds');
+  var $timerEndDate = timer.querySelector('.timer__end-date'); // вызываем функцию countdownTimer
+
+  countdownTimer(); // вызываем функцию countdownTimer каждую секунду
+
+  timerId = setInterval(countdownTimer, 1000); // дата на ленде
+
+  var today = document.querySelectorAll('.today');
+
+  for (var i = 0; i < today.length; i++) {
+    today[i].textContent = deadline.getDate() + ' ' + arrMount[deadline.getMonth()] + ' в ' + leadingZero(deadline.getHours()) + ':' + leadingZero(deadline.getMinutes()) + ' по МСК';
+  }
+}
